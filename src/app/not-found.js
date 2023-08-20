82ba156ab4e69781f41e3468c89976e990a5a844
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-20 22:39:47 +0600

    not-found page added

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-2xl text-red-600 h-[100vh] flex flex-col justify-center items-center'>
+            <h2 className='mb-4'>404</h2>
+            <p>Not found</p>
+        </div>
+    );
+};
+
+export default NotFound;
