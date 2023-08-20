a1644087d87ad50770e17ff53ec6c8e2df0d89d5
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-20 19:41:45 +0600

    dynamic routes added

---
+++ src/app/dashboard/blogs/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleBlog = () => {
+    return (
+        <div>
+            <h2>Single Blog in dashboard</h2>
+        </div>
+    );
+};
+
+export default SingleBlog;
--- src/app/dashboard/blogs/page.js
+++ src/app/dashboard/blogs/page.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+    title: 'Blogs | Dashboard | Practice NextJS',
+    description: 'Practice NextJS',
+  }
+
 const DashboardBlogs = () => {
     return (
         <div>
